bd7d2ce27672ce8e718bd111cf418b7f286088a8
Sam <samuelhamilton2017@u.northwestern.edi>
2020-03-04 11:54:27 -0600

    added entropy calculation code

--- code/Cardiac_Arrest/rrdata.R
+++ code/Cardiac_Arrest/rrdata.R
@@ -2,18 +2,20 @@ library(TSEntropies)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
-
+library(plyr)
 #Calculates entropy using sliding window approach
 CalcEnSlide <- function(entmeasure = "FastSampEn",df,grouplist,r_thrsh = .2,size = 200, step = 1) {
   
   #calculate  sd for all ts 
   allsd <- sd(df$value,na.rm = T)
+  print(allsd)
   #Calculates entropy for a TS across a set of sliding windows
-  calc_En_slidingwindow <- function(entmeasure,size,ofint,step,r_thresh,allsd) {
+  calc_En_slidingwindow <- function(entmeasure,varofint,size,step,r_thresh,allsd) {
     slidingentlist <- c() #init list 
-    for(i in seq(from = 1, to = (length(ofint) - size), by = step)){
+    print(length(varofint))
+    for(i in seq(from = 1, to = (length(varofint) - size), by = step)){
       
-      window <- ofint[i:(i+size)]
+      window <- varofint[i:(i+size)]
       EN <- do.call(entmeasure,list(window, r = r_thresh*allsd))
       slidingentlist <- append(slidingentlist,EN)
     }
@@ -26,7 +28,7 @@ CalcEnSlide <- function(entmeasure = "FastSampEn",df,grouplist,r_thrsh = .2,size
              grouplist,
              here(summarise),
              entropy = median(calc_En_slidingwindow(entmeasure = entmeasure,
-                                                    ofint = value,
+                                                    varofint = value,
                                                     size = size,
                                                     step = step,
                                                     r_thresh = r_thrsh,
@@ -37,58 +39,43 @@ CalcEnSlide <- function(entmeasure = "FastSampEn",df,grouplist,r_thrsh = .2,size
   return(k)
 }
 
+
 # Heart Rate Data
-hr <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/Chartevents_211_hr.csv")
+rr <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/Chartevents_618_rr.csv")
 
 # Patient IDs
 ca_ids <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/ca_ids.csv")
 
-hr <- merge(hr,ca_ids,by = "subject_id")
+nrow(unique(data.frame(rr$subject_id)))
+nrow(unique(data.frame(rr$hadm_id))) # checking that there are unique admissions per patient - looks good!
 
+rr_counts <- rr %>% group_by(subject_id) %>% dplyr::count(num_obs = n())
 
- nrow(unique(data.frame(hr$subject_id)))
-nrow(unique(data.frame(hr$hadm_id))) # checking that there are unique admissions per patient - looks good!
+rr_counts_200 <- rr_counts %>% filter(num_obs >= 410)
 
-
-hr_counts <- hr %>% group_by(subject_id) %>% dplyr::count(num_obs = n())
-
-hr_counts_200 <- hr_counts %>% filter(num_obs >= 220)
-
-hr <- hr %>% 
-  filter(subject_id %in% hr_counts_200$subject_id)
+rr <- rr %>% filter(subject_id %in% rr_counts_200$subject_id)
 ## Data Pre-processing
 
-
 # CHF Patients
-hr$charttime_conv <- strptime(as.character(hr$charttime), format = "%Y-%m-%d %H:%M:%S", tz = "EST")
-hr$charttime_conv <- as.POSIXct(hr$charttime_conv, tz="EST")
+rr$charttime_conv <- strptime(as.character(rr$charttime), format = "%Y-%m-%d %H:%M:%S", tz = "EST")
+rr$charttime_conv <- as.POSIXct(rr$charttime_conv, tz="EST")
 
-hr$value <- as.numeric(as.character(hr$value))
+rr$value <- as.numeric(as.character(rr$value))
+rr = rr %>% inner_join(ca_ids, by = c("subject_id","hadm_id"))
 
-
-
-hr <- hr %>% filter(!value == "NULL") %>% 
-  filter(value > 0)
+rr <- rr %>% filter(!value == "NULL")
 
 
 
-#Take the min, max, mean, and sd when measurements occur within the same time interval (if we want this step!)
-
-#hr <- hr %>% 
-# group_by(subject_id, charttime_conv) %>% 
-#   mutate(max_hr = max(value, na.rm = TRUE)) %>%
-#   mutate(min_hr = min(value, na.rm = TRUE)) %>%
-#   mutate(mean_hr = mean(value, na.rm = TRUE)) %>% 
-#   mutate(sd_hr = sd(value, na.rm = TRUE)) 
-
+#Take the min, max, mean, and sd when measurements occur within the same time interval (if we want
+#rr <- rr %>% group_by(subject_id, charttime_conv) %>% mutate(max_rr = max(value, na.rm = TRUE)) %>% mutate(min_rr = min(value, na.rm = TRUE)) %>% mutate(mean_rr = mean(value, na.rm = TRUE)) %>% mutate(sd_rr = sd(value, na.rm = TRUE)) 
 
 ## Create time plots
-
 # x = subject_id
-plot_hr <- function(x) { 
+plot_rr <- function(x) { 
   
   # arrange by Date
-  df <- hr %>% filter(subject_id == x) %>% arrange(charttime_conv)
+  df <- rr %>% filter(subject_id == x) %>% arrange(charttime_conv)
   
   #Title: create a vector that will contain the ID number and can be used to title the    graph by patient ID 
   title <- x
@@ -97,39 +84,42 @@ plot_hr <- function(x) {
   first <- first(df$charttime_conv)
   last <- last(df$charttime_conv)
   
-  hr <- df %>% filter(!is.na(value)) %>% select(subject_id, charttime_conv, value) %>%
+  rr <- df %>% filter(!is.na(value)) %>% select(subject_id, charttime_conv, value) %>%
     ggplot(aes(x = charttime_conv, y = value)) + 
     geom_point(color = "slateblue") + 
     geom_line(color = "slateblue") +
-    #geom_hline(yintercept=3) + #could demark dangerous (low or high) HRs
-    scale_y_continuous(name = "BMP") + #define the y-axis limits
+    #geom_hline(yintercept=3) + #could demark dangerous (low or high) rrs
+    scale_y_continuous(name = "Resp Rate") + #define the y-axis limits
     #scale_x_continuous(name = "Time", date_break = "1 hour", date_labels = "%H", limits = c(first, last)) + #, limits = c(-6, 0), breaks = c(-6:0)) +
     scale_x_datetime(name = "Time", date_break = "6 hour", date_labels = "%H", limits = c(first, last)) +
     ggtitle(paste0('ID:', title))  #add a title to identify patients 
   
   #remove legend and x-axis
-  hr <- hr  + theme(legend.position = "none")
+  rr <- rr  + theme(legend.position = "none")
   
   #remove gray background and add a border around the plot
-  hr <- hr + theme(panel.border = element_rect(fill=NA,color="black", size=0.5,
+  rr <- rr + theme(panel.border = element_rect(fill=NA,color="black", size=0.5,
                                                linetype="solid"))
   
   
-  hr <- hr + theme_bw()
+  rr <- rr + theme_bw()
   
-  return(hr)
+  return(rr)
 }
 
 
-
-m <- CalcEnSlide(df = hr,grouplist = c("hospital_expire_flag","subject_id"), size = 200,step = 1,r_thrsh = .5)
-
-plot_hr(2734) # low ent 
-plot_hr(6020) #high ent
-plot_hr(6543)
-m$hospital_expire_flag <- as.factor(m$hospital_expire_flag)
-ggplot(m,aes(x = hospital_expire_flag, y = entropy, fill = hospital_expire_flag)) +
-  geom_boxplot()
+m <- CalcEnSlide(df = rr, 
+                 grouplist = c("hospital_expire_flag","subject_id","hadm_id"), 
+                 step = 1,
+                 r_thrsh = .15)
 
 lm1 <- glm(hospital_expire_flag ~ entropy, family = "binomial", data = m)
 summary(lm1)
+
+m$hospital_expire_flag <- as.factor(m$hospital_expire_flag)
+ggplot(m,aes(x = hospital_expire_flag, y = entropy, fill = hospital_expire_flag)) +
+  geom_boxplot() +
+  geom_point()
+
+plot_rr(20866) #high ent
+plot_rr(6469) # low entropy
--- code/Cardiac_Arrest/tempcode.R
+++ code/Cardiac_Arrest/tempcode.R
@@ -1,7 +1,9 @@
-library(TSEntropies)
-library(ggplot2)
+library(TSEntropies) 
 library(dplyr)
+library(lubridate)
+library(ggplot2)
 library(plyr)
+
 #Calculates entropy using sliding window approach
 CalcEnSlide <- function(entmeasure = "FastSampEn",df,grouplist,r_thrsh = .2,size = 200, step = 1) {
   
@@ -38,12 +40,45 @@ CalcEnSlide <- function(entmeasure = "FastSampEn",df,grouplist,r_thrsh = .2,size
   return(k)
 }
 
+# Heart Rate Data
+temp <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/Chartevents_678_temp_F.csv")
 
+# Patient IDs
+ca_ids <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/ca_ids.csv")
+
+nrow(unique(data.frame(temp$subject_id)))
+nrow(unique(data.frame(temp$hadm_id))) # checking that there are unique admissions per patient - looks good!
+
+
+temp_counts <- temp %>% group_by(hadm_id) %>% dplyr::count(num_obs = n())
+
+temp_counts_200 <- temp_counts %>% filter(num_obs >= 200)
+temp <- temp %>% 
+  filter(hadm_id %in% temp_counts_200$hadm_id)
+#We will need to check the time interval/perhaps standardize by time in some fashion (ie: if multiple measurements within the same time frame, do we want to take the mean, max, min? min or max he
+## Data Pre-processing
+
+# CHF Patients
+temp$charttime_conv <- strptime(as.character(temp$charttime), format = "%Y-%m-%d %H:%M:%S", tz = "EST")
+temp$charttime_conv <- as.POSIXct(temp$charttime_conv, tz="EST")
+
+temp$value <- as.numeric(as.character(temp$value))
+
+temp <- temp %>% inner_join(ca_ids, by = c("subject_id","hadm_id"))
+
+temp <- temp %>% filter(!value == "NULL")
+
+
+
+#Take the min, max, mean, and sd when measurements occur within the same time interval (if we want this step!)
+#temp <- temp %>% group_by(subject_id, charttime_conv) %>% mutate(max_temp = max(value, na.rm = TRUE)) %>% mutate(min_temp = min(value, na.rm = TRUE)) %>% mutate(mean_temp = mean(value, na.rm = TRUE)) %>% mutate(sd_temp = sd(value, na.rm = TRUE)) 
+
+## Create time plots
 # x = subject_id
-plot_sbp <- function(x) { 
+plot_temp <- function(x) { 
   
   # arrange by Date
-  df <- sbp %>% filter(subject_id == x) %>% arrange(charttime_conv)
+  df <- temp %>% filter(subject_id == x) %>% arrange(charttime_conv)
   
   #Title: create a vector that will contain the ID number and can be used to title the    graph by patient ID 
   title <- x
@@ -52,55 +87,30 @@ plot_sbp <- function(x) {
   first <- first(df$charttime_conv)
   last <- last(df$charttime_conv)
   
-  sbp <- df %>% filter(!is.na(value)) %>% select(subject_id, charttime_conv, value) %>%
+  temp <- df %>% filter(!is.na(value)) %>% select(subject_id, charttime_conv, value) %>%
     ggplot(aes(x = charttime_conv, y = value)) + 
     geom_point(color = "slateblue") + 
     geom_line(color = "slateblue") +
-    #geom_hline(yintercept=3) + #could demark dangerous (low or high) sbps
-    scale_y_continuous(name = "mmHg") + #define the y-axis limits
+    #geom_hline(yintercept=3) + #could demark dangerous (low or high) temps
+    scale_y_continuous(name = "Degrees F") + #define the y-axis limits
     #scale_x_continuous(name = "Time", date_break = "1 hour", date_labels = "%H", limits = c(first, last)) + #, limits = c(-6, 0), breaks = c(-6:0)) +
     scale_x_datetime(name = "Time", date_break = "6 hour", date_labels = "%H", limits = c(first, last)) +
-    ggtitle(paste0('ID:', title)) + #add a title to identify patients  
-    coord_cartesian(ylim = c(0,300))
+    ggtitle(paste0('ID:', title))  #add a title to identify patients 
   
   #remove legend and x-axis
-  sbp <- sbp  + theme(legend.position = "none")
+  temp <- temp  + theme(legend.position = "none")
   
   #remove gray background and add a border around the plot
-  sbp <- sbp + theme(panel.border = element_rect(fill=NA,color="black", size=0.5,
-                                                 linetype="solid"))
+  temp <- temp + theme(panel.border = element_rect(fill=NA,color="black", size=0.5,
+                                                   linetype="solid"))
   
   
-  sbp <- sbp + theme_bw()
+  temp <- temp + theme_bw()
   
-  return(sbp)
+  return(temp)
 }
 
-# Heart Rate Data
-sbp <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/Chartevents_51_arterial_sbp.csv")
-
-# Patient IDs
-ca_ids <- read.csv("/Users/samuelhamilton/Downloads/Mimic_HSIP/Mimic_Data/Cardiac_Arrest/ca_ids.csv")
-
-sbp_counts <- sbp %>% group_by(hadm_id) %>% dplyr::count(num_obs = n())
-
-sbp_counts_200 <- sbp_counts %>% filter(num_obs >= 210)
-sbp <- sbp %>% 
-  filter(hadm_id %in% sbp_counts_200$hadm_id)
-
-# CHF Patients
-sbp$charttime_conv <- strptime(as.character(sbp$charttime), format = "%Y-%m-%d %H:%M:%S", tz = "EST")
-sbp$charttime_conv <- as.POSIXct(sbp$charttime_conv, tz="EST")
-
-sbp$value <- as.numeric(as.character(sbp$value)) 
-
-sbp = sbp %>% inner_join(ca_ids, by = c("subject_id","hadm_id"))
-
-sbp <- sbp %>% filter(!value == "NULL") %>% filter(!is.na(value)) %>% 
-  filter(value > 0)
-
-
-m <- CalcEnSlide(df = sbp, 
+m <- CalcEnSlide(df = temp, 
                  grouplist = c("hospital_expire_flag","subject_id","hadm_id"), 
                  size = 200,
                  step = 1,
@@ -115,4 +125,7 @@ m$hospital_expire_flag <- as.factor(m$hospital_expire_flag)
 ggplot(m,aes(x = hospital_expire_flag, y = entropy, fill = hospital_expire_flag)) +
   geom_boxplot() +
   geom_point()
-
+plot_temp(27850)
+plot_temp(849) # high entropy
+plot_temp(7223) # low entropy
+plot_temp(849)# low entropy
